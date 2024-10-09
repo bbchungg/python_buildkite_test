a83256d9bc0500f7bc49f9b192edc5da0962660e
Brian Chung <brianjaychung@gmail.com>
2024-10-09 11:22:19 -0700

    fix indent issue

--- count.py
+++ count.py
@@ -1,7 +1,7 @@
 def print_even_numbers():
 
-for num in range(1, 11):
-    if num % 2 == 0:
-        print(num)
+    for num in range(1, 11):
+        if num % 2 == 0:
+            print(num)
 
 print_even_numbers()
